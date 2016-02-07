86f903b0942097b2009eff9ce296df59797b4333
yassu <mathyassu@gmail.com>
2016-02-08 00:01:07 +0900

    get_dates

--- src/hyuki_graph.py
+++ src/hyuki_graph.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 # coding: UTF-8
+
 import os
+import datetime
+import pprint
 
 def get_children_dirs(path):
     for (_, dirs, _) in os.walk(path):
         for d in dirs:
             yield os.path.abspath(d)
 
-print(list(get_children_dirs('.')))
+def get_dates():
+    dates = []
+    today = datetime.datetime.today()
+    for days in [7 - days for days in range(7 + 1)]:
+        yield today - datetime.timedelta(days=days)
+
+pprint.pprint(list(get_dates()))
+print(len(list(get_dates())))
