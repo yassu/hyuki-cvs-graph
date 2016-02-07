8f209689328657441bd26d3e71efa21c61c52b31
yassu <mathyassu@gmail.com>
2016-02-08 01:39:16 +0900

    over write with bug

--- src/hyuki_graph.py
+++ src/hyuki_graph.py
@@ -5,6 +5,7 @@ import os
 import datetime
 import pprint
 import subprocess
+from PIL import Image, ImageDraw
 
 def get_commit_numbers(path):
     os.chdir(path)
@@ -20,7 +21,7 @@ def get_commit_numbers(path):
     for date in get_dates():
         year, month, day = date.year, date.month, date.day
         _format = "%04d-%02d-%02d" % (year, month, day)
-        numbers[date] = log.count(_format)
+        numbers[datetime.datetime(year, month, day)] = log.count(_format)
     return numbers
 
 def get_children_dirs(path):
@@ -34,15 +35,40 @@ def get_cvs_dirs(path):
             yield p
 
 def get_dates():
-    dates = []
     today = datetime.datetime.today()
     for days in [7 - days for days in range(7 + 1)]:
-        yield today - datetime.timedelta(days=days)
+        date = today - datetime.timedelta(days=days)
+        yield datetime.date(date.year, date.month, date.day)
 
 def main():
     commits = dict()
-    for path in list(get_cvs_dirs('.')):
+    projects = list(get_cvs_dirs('.'))
+    for path in projects:
         commits[path] = get_commit_numbers(path)
-    pprint.pprint(commits)
+
+    right_span = 10
+    left_span = 10
+    cell_width = 40
+    cell_height = cell_width
+    image_matrix = Image.new('RGB',
+        (7 * cell_width, cell_height * (len(projects) - 1 + 1)),
+        (255, 255, 255))
+    draw = ImageDraw.Draw(image_matrix)
+
+    for i, date in enumerate(get_dates()):
+        # write date as text
+        date_text = "%04d/%02d/%02d" % (date.year, date.month, date.day)
+        draw.text((left_span + i * cell_width, 0), date_text, fill='#000000')
+
+        for j, project in enumerate(projects):
+            if commits[project][date] != 0:
+                draw.rectangle(
+                    (
+                        (i * cell_width, (j + 1) * cell_height),
+                        ((i + 1) * cell_width, (j + 2) * cell_height)
+                    ),
+                    fill=(0, 0, 256), outline=(0, 0, 0))
+
+    image_matrix.show()
 
 main()
