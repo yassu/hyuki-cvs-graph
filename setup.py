eb76fa43f6dd85e3912c185c13e40939de344bfa
yassu <mathyassu@gmail.com>
2016-02-09 01:36:38 +0900

    fix classifiers

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from hyuki_graph import __VERSION__
 classifiers = [
     'Development Status :: 5 - Production/Stable',
     'Environment :: Console',
-    # 'License :: OSI Approved :: Apache Software License',
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python',
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
