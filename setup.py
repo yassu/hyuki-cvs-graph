269c730ea3f77dea6fa8a4543354313d459a349c
yassu <mathyassu@gmail.com>
2016-02-08 22:24:07 +0900

    pip

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    # 'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Utilities',
+]
+
+setup(
+    name='hyuki-cvs-graph',
+    version='0.0.1',
+    description='view graph for working log',
+    # long_description=open('README.rst').read(),
+    author='Yassu',
+    author_email='mathyassu@gmail.com',
+    url='https://github.com/yassu/hyuki-cvs-graph',
+    classifiers=classifiers,
+    entry_points="""
+       [console_scripts]
+       hgraph = hyuki_graph.hyuki_graph:main
+    """,
+)
+
