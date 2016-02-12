3bb29705437182776deebcdbcfa3fbd66ecb6b80
yassu <mathyassu@gmail.com>
2016-02-13 02:57:44 +0900

    bump ver

--- hyuki_graph/hyuki_graph.py
+++ hyuki_graph/hyuki_graph.py
@@ -10,10 +10,10 @@ import subprocess
 import re
 from optparse import OptionParser
 import json
-# import yaml
+import yaml
 from collections import defaultdict
 
-__VERSION__ = '0.2.4'
+__VERSION__ = '0.2.5'
 
 DEFAULT_NUMBER_OF_DAY = 7
 DEFAULT_MEDIUM_SEP = 10
