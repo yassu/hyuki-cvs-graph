bcefe75808a6a1e30855fcc17e1a6f59cd9f6ddb
yassu <mathyassu@gmail.com>
2016-02-13 01:23:46 +0900

    requires => install_requires and bump_version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-__VERSION__ = '0.2.0'
+__VERSION__ = '0.2.1'
 
 classifiers = [
     'Development Status :: 3 - Alpha',
@@ -24,7 +24,7 @@ setup(
     author='Yassu',
     author_email='mathyassu@gmail.com',
     url='https://github.com/yassu/hyuki-cvs-graph',
-    requires=['docutils', 'PyYAML'],
+    install_requires=['docutils', 'PyYAML'],
     classifiers=classifiers,
     entry_points="""
        [console_scripts]
